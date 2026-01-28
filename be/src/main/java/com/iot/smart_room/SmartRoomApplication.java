1ddbd2478ce3651ea986fa4610c4251c37f12a8f
hoanglong2534 <phamxuanhoanglong@gmail.com>
2026-01-28 21:13:50 +0700

    feat(BE): init

---
+++ be/src/main/java/com/iot/smart_room/SmartRoomApplication.java
@@ -0,0 +1,13 @@
+package com.iot.smart_room;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartRoomApplication.class, args);
+	}
+
+}
---
+++ be/src/test/java/com/iot/smart_room/SmartRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iot.smart_room;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
